316ad1470a10b235d81d23be899e35d4a53526ce
shafirasalim <shafira03excellent@gmail.com>
2026-08-04 17:04:12 +0700

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,10 +7,6 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
 
     public function boot(): void
     {
